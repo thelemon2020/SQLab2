c660b1bc40c583271cbb36c70a37ea2c1d34e536
Daniel Ranieri Negri <negri.ranieri@gmail.com>
2020-09-25 11:21:11 -0400

    comitting testing with attributes and one methos

--- SQLab2/Triangle.cpp
+++ SQLab2/Triangle.cpp
@@ -1,1 +1,8 @@
 #include "Triangle.h"
+
+int Triangle::AngleCalc(int angleA, int angleB)
+{
+	int angleSum = 180;
+	angleSum = -(angleA + angleB);
+	return angleSum;
+}
--- SQLab2/Triangle.h
+++ SQLab2/Triangle.h
@@ -1,5 +1,17 @@
 #pragma once
 class Triangle
 {
+private:
+
+	int sideA;
+	int sideB;
+	int sideC;
+	int angleA;
+	int angleB;
+	int angleC;
+
+public:
+
+	int AngleCalc(int angleA, int angleB);
 };
 
