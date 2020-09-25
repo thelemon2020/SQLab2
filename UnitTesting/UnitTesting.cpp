2062f77131d34a10f54245a701c7952b92b3d477
thelemon2020 <53379286+thelemon2020@users.noreply.github.com>
2020-09-25 11:07:36 -0400

    Added Unit Testing

---
+++ UnitTesting/UnitTesting.cpp
@@ -0,0 +1,16 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTesting
+{
+	TEST_CLASS(UnitTesting)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
