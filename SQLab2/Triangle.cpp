af2b4cf6c6661e1233d33c0fab32bc8958dbd33d
Daniel Ranieri Negri <negri.ranieri@gmail.com>
2020-09-25 11:46:21 -0400

    Restarting over with the principles of TDD in mind

--- SQLab2/Triangle.cpp
+++ SQLab2/Triangle.cpp
@@ -1,18 +1,1 @@
 #include "Triangle.h"
-
-int Triangle::AngleCalc(int angleA, int angleB)
-{
-	int angleSum = 180;
-	angleSum = -(angleA + angleB);
-	return angleSum;
-}
-
-Triangle::Triangle(int sideA, int sideB, int sideC, int angleA, int angleB, int angleC)
-{
-	this->sideA = sideA;
-	this->sideB = sideB;
-	this->sideC = sideC;
-	this->angleA = angleA;
-	this->angleB = angleB;
-	this->angleC = angleC;
-}
--- SQLab2/Triangle.h
+++ SQLab2/Triangle.h
@@ -1,18 +1,5 @@
 #pragma once
 class Triangle
 {
-private:
-
-	int sideA;
-	int sideB;
-	int sideC;
-	int angleA;
-	int angleB;
-	int angleC;
-
-public:
-
-	Triangle(int sideA, int sideB, int sideC, int angleA, int angleB, int angleC);
-	int AngleCalc(int angleA, int angleB);
 };
 
