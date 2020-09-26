76e39a564426c6210ee7b54c09204f9c32f51cf6
Hongxuan Yue <hyue1758@conestogac.on.ca>
2020-09-25 22:08:18 -0400

    Commit after the passing test for findRemainingAngle()

--- UnitTesting/UnitTesting.cpp
+++ UnitTesting/UnitTesting.cpp
@@ -16,6 +16,7 @@ namespace UnitTesting
 			Triangle testTriangle(10,10);
 			int third_angle_result = testTriangle.findRemainingAngle(30, 30);
 			Assert::AreEqual(120, third_angle_result);
+			//The result is green.
 		}
 	};
 }
