8ab6125d474ef236b4b11fd1391260e1e9f45008
thelemon2020 <53379286+thelemon2020@users.noreply.github.com>
2020-09-26 14:16:40 -0400

    Started new project
    Created new set of files to make sure they work across multiple computers

---
+++ SQLab2/Triangle.cpp
@@ -0,0 +1,31 @@
+#include "Triangle.h"
+Triangle::Triangle(int a, int b)
+{
+	sideA = a;
+	sideB = b;
+}
+
+double Triangle::PythagFormula(int a, int b)
+{
+	return 0;
+}
+
+double Triangle::FindArea(int a, int b)
+{
+	return 0;
+}
+
+int Triangle::findRemainingAngle(int a, int b)
+{
+	/*
+	if (a + b >= 180)
+	{
+		return 0;//Validation of a and b. Simply return 0 if
+	}
+	*/
+
+	//Comment out this section to purposely fail a test
+
+
+	return 180 - a - b;
+}
---
+++ SQLab2/test.cpp
@@ -0,0 +1,6 @@
+#include "Triangle.h"
+
+int main()
+{
+
+}
