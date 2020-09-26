835b31277e2d1d15aa6c2da22796c0d82f0bcbcb
Hongxuan Yue <hyue1758@conestogac.on.ca>
2020-09-25 21:30:29 -0400

    Failed Test for the method findRemainingAngle()

--- UnitTesting/UnitTesting.cpp
+++ UnitTesting/UnitTesting.cpp
@@ -11,9 +11,11 @@ namespace UnitTesting
 	{
 	public:
 		
-		TEST_METHOD(TestMethod1)
+		TEST_METHOD(Test_findRemainingAngle)
 		{
-
+			Triangle testTriangle(10,10);
+			int third_angle_result = testTriangle.findRemainingAngle(30, 30);
+			Assert::AreEqual(120, third_angle_result);
 		}
 	};
 }
